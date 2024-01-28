f317bb33cc6e1c267b60547dbcb7b08f0dc7557b
KKiiim <jinrui_0322@163.com>
2024-01-28 23:12:36 +0800

    add clang-format

--- test/gtest.cpp
+++ test/gtest.cpp
@@ -1,40 +1,40 @@
-#include <gtest/gtest.h>
 #include <fstream>
+#include <gtest/gtest.h>
 #include <string>
 
-bool compareFiles(const std::string& file1, const std::string& file2) {
-    std::ifstream fileStream1(file1);
-    std::ifstream fileStream2(file2);
+bool compareFiles(const std::string &file1, const std::string &file2) {
+  std::ifstream fileStream1(file1);
+  std::ifstream fileStream2(file2);
 
-    std::string line1, line2;
-    while (std::getline(fileStream1, line1) && std::getline(fileStream2, line2)) {
-        if (line1 != line2) {
-            return false;
-        }
+  std::string line1, line2;
+  while (std::getline(fileStream1, line1) && std::getline(fileStream2, line2)) {
+    if (line1 != line2) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
 TEST(FileComparisonTest, CompareSelect) {
-    std::string outputFile = "/tmp/selectOutput.txt";
-    std::string answerFile = "selectAnswer.txt";
+  std::string outputFile = "/tmp/selectOutput.txt";
+  std::string answerFile = "selectAnswer.txt";
 
-    EXPECT_TRUE(compareFiles(outputFile, answerFile));
+  EXPECT_TRUE(compareFiles(outputFile, answerFile));
 }
 TEST(FileComparisonTest, ComparePoll) {
-    std::string outputFile = "/tmp/selectOutput.txt";
-    std::string answerFile = "pollAnswer.txt";
+  std::string outputFile = "/tmp/selectOutput.txt";
+  std::string answerFile = "pollAnswer.txt";
 
-    EXPECT_TRUE(compareFiles(outputFile, answerFile));
+  EXPECT_TRUE(compareFiles(outputFile, answerFile));
 }
 TEST(FileComparisonTest, CompareEpoll) {
-    std::string outputFile = "/tmp/selectOutput.txt";
-    std::string answerFile = "epollAnswer.txt";
+  std::string outputFile = "/tmp/selectOutput.txt";
+  std::string answerFile = "epollAnswer.txt";
 
-    EXPECT_TRUE(compareFiles(outputFile, answerFile));
+  EXPECT_TRUE(compareFiles(outputFile, answerFile));
 }
 
-int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
